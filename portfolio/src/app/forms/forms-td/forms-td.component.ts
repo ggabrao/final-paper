b2123e2e024486b98a5b7eac413c406651f57dd8
Operations Linux <gabriel.abrao@hotmail.com>
2023-10-25 16:03:48 -0300

    td form draft

--- portfolio/src/app/forms/forms-td/forms-td.component.ts
+++ portfolio/src/app/forms/forms-td/forms-td.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'frm-td',
   templateUrl: './forms-td.component.html',
-  styleUrls: ['./forms-td.component.scss']
+  styleUrls: ['./forms-td.component.scss'],
 })
 export class FormsTdComponent {
 
+
+  signUp() {
+   
+  }
 }
