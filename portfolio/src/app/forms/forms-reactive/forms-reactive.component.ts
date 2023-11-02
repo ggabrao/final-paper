e799199d2dcb7d9b5940d590a68a27dd743d5c92
Gabriel G Abrao <gabriel.abrao@hotmail.com>
2023-11-02 08:47:34 -0300

    Starting reactive forms class

--- portfolio/src/app/forms/forms-reactive/forms-reactive.component.ts
+++ portfolio/src/app/forms/forms-reactive/forms-reactive.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IUser } from '../user.model';
 
 @Component({
   selector: 'frm-reactive',
   templateUrl: './forms-reactive.component.html',
   styleUrls: ['./forms-reactive.component.scss']
 })
-export class FormsReactiveComponent {
+export class FormsReactiveComponent implements OnInit {  
+  user:IUser | undefined;
+  userForm!: FormGroup; 
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({});
+  } 
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- portfolio/src/app/shared/shared.module.ts
+++ portfolio/src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { MaterialModule } from './material.module';
 
 
 @NgModule({
-  exports: [CommonModule, MaterialModule, FormsModule],
+  exports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
