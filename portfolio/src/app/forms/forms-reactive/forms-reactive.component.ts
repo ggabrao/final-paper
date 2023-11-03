7527708c304a78ec90e3c759f6a1f1c2d75ce279
Operations Linux <gabriel.abrao@hotmail.com>
2023-11-03 09:41:26 -0300

    first setp up with the reactive form

--- portfolio/src/app/forms/forms-reactive/forms-reactive.component.ts
+++ portfolio/src/app/forms/forms-reactive/forms-reactive.component.ts
@@ -5,13 +5,13 @@ import { IUser } from '../user.model';
 @Component({
   selector: 'frm-reactive',
   templateUrl: './forms-reactive.component.html',
-  styleUrls: ['./forms-reactive.component.scss']
+  styleUrls: ['./forms-reactive.component.scss'],
 })
-export class FormsReactiveComponent implements OnInit {  
-  user:IUser | undefined;
-  userForm!: FormGroup; 
+export class FormsReactiveComponent implements OnInit {
+  user: IUser | undefined;
+  userForm!: FormGroup;
 
-  constructor(private fb:FormBuilder) {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.userForm = this.fb.group({
@@ -20,20 +20,24 @@ export class FormsReactiveComponent implements OnInit {
       email: null,
       address: null,
       password: null,
-      confirmPassword: null,   
-      agreement: false
-    })
-  } 
+      confirmPassword: null,
+      agreement: false,
+    });
+  }
 
+  populateTest(): void {
+    this.userForm.setValue({
+      name: 'Gabriel',
+      phone: null,
+      email: 'gabriel@test.com',
+      address: null,
+      password: 1234,
+      confirmPassword: 1234,
+      agreement: false,
+    });
+  }
 
-
-
-
-
-
-
-
-
-
-
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
 }
