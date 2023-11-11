b6d12ce51488dd9498b3b5b45c1bb7852ebf69e1
Operations Linux <gabriel.abrao@hotmail.com>
2023-11-11 15:52:20 -0300

    strating the table

--- portfolio/src/app/courses/courses.service.ts
+++ portfolio/src/app/courses/courses.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ICourse } from './course.model';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CoursesService {  
+export class CoursesService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getCourses(): Observable<ICourse[]> {
     return this.http.get<ICourse[]>('api/courses');
@@ -18,6 +18,20 @@ export class CoursesService {
     return this.http.get<ICourse>(`api/courses/${id}`);
   }
 
-  
+  addCourse(newCourse: ICourse): Observable<ICourse> {
+    return this.http.post<ICourse>('api/courses', newCourse, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    })
+  };
+
+  deleteCourse(id: number): Observable<ICourse> {
+    return this.http.delete<ICourse>(`api/courses`).pipe(tap(_ => console.log(`deleted course id=${id}`)),)
+   
+  }
 
 }
+
+
+
