4a4e1f01a95278982dedf60483de636a4b666c63
Operations Linux <gabriel.abrao@hotmail.com>
2023-11-13 14:05:38 -0300

    http methos and buttons working

--- portfolio/src/app/courses/edit/edit.component.ts
+++ portfolio/src/app/courses/edit/edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { CoursesService } from '../courses.service';
 import { ICourse } from '../course.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'crs-edit',
@@ -12,14 +13,21 @@ export class EditComponent implements OnInit{
 
   selectedCourse!: ICourse;
 
-  constructor(private route:ActivatedRoute, private dataService: CoursesService) {}
+  constructor(private route:ActivatedRoute, private dataService: CoursesService, private router:Router) {}
 
   ngOnInit():void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.dataService.getCourse(id).subscribe(data => this.selectedCourse = data);
   }
 
+  onCancel() {
+      this.router.navigate(['/courses']);
+    }
 
+  onSubmit():void {    
+    this.dataService.updateCourse(this.selectedCourse).subscribe(() => this.router.navigate(['/courses']));
+  }
+    
 
   
 }
