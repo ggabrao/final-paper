c2eda8365fbfe133ee033527cc120d07ac4df876
Operations Linux <gabriel.abrao@hotmail.com>
2023-11-15 10:31:27 -0300

    Implemented loading spinner

---
+++ portfolio/src/app/spinner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  template: '<mat-spinner class="spinner"></mat-spinner>',
+  styles: '.spinner { position:absolute; top:50%; left:50% }'
+})
+export class SpinnerComponent {}
