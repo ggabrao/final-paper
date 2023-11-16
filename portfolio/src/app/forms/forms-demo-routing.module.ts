3eeafb90c0bdcb5808d67a42371f0c213e4f99bc
Operations Linux <gabriel.abrao@hotmail.com>
2023-11-16 12:03:50 -0300

    Many adjusts on the forms page

--- portfolio/src/app/forms/forms-demo-routing.module.ts
+++ portfolio/src/app/forms/forms-demo-routing.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsIntroComponent } from './forms-intro.component';
-import { FormsTdComponent } from './forms-td/forms-td.component';
-import { FormsReactiveComponent } from './forms-reactive/forms-reactive.component';
 
 const routes: Routes = [
       {
         path: '',
-        component: FormsIntroComponent,
-        children: [{
-          path: 'template-driven',
-          component: FormsTdComponent,
-        },
-        {
-          path: 'reactive',
-          component: FormsReactiveComponent,
-        }          
-        ]
+        component: FormsIntroComponent
       }      
 ];
 
