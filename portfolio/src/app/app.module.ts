618012188ce6780662b400177f55a0c620331d27
Gabriel <gabriel.abrao@hotmail.com>
2023-11-28 10:08:33 -0300

    fixes in the scructure

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-import { NavComponent } from './header/nav/nav.component';
+import { NavComponent } from './header/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { IntroComponent } from './informations/intro/intro.component';
 import { WhoComponent } from './introduction/who/who.component';
