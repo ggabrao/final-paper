e6a8e8461861662f92943b90fea270ee54e5531f
Gabriel <gabriel.abrao@hotmail.com>
2023-12-08 10:09:32 -0300

    temp

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, Event } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { FooterService } from './footer.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'portfolio';
   loading = true;
+  showFooter!: boolean;
+  subscription!: Subscription;
 
-  constructor(public router: Router) {
+
+  constructor(public router: Router, private footerService: FooterService) {
     router.events.subscribe((routerEvent: Event) => this.checkRouterEvent(routerEvent));
   }
-
 
   checkRouterEvent(routerEvent: Event): void {
     if (routerEvent instanceof NavigationStart) {
@@ -24,5 +28,14 @@ export class AppComponent {
       this.loading = false;
     }
   }
+
+  ngOnInit(): void {
+    this.subscription = this.footerService.showFooter.subscribe(value => this.showFooter = value);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 
 }
