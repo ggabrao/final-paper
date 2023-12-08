fb0bcdc9a1cdc7288fefa33635a59f8f4bf0a516
Gabriel <gabriel.abrao@hotmail.com>
2023-12-08 10:05:33 -0300

    temp

--- portfolio/src/app/footer/footer.component.ts
+++ portfolio/src/app/footer/footer.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { FooterService } from '../footer.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
   myDate: Date;
+  showFooter!: boolean;
+  subscription!: Subscription;
 
-  constructor() {
+  constructor(private footerService: FooterService) {
     this.myDate = new Date();
   }
+
+  ngOnInit(): void {
+    this.subscription = this.footerService.showFooter.subscribe(value => this.showFooter = value);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 
 }
--- portfolio/src/app/forms/forms-intro.component.ts
+++ portfolio/src/app/forms/forms-intro.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FooterService } from '../footer.service';
 
 @Component({
   selector: 'frm-intro',
   templateUrl: './forms-intro.component.html',
   styleUrls: ['./forms-intro.component.scss']
 })
-export class FormsIntroComponent {
+
+export class FormsIntroComponent implements OnInit, OnDestroy {
+
+constructor(private footerService:FooterService) {}
+
+  ngOnInit(): void {
+    this.footerService.hide();
+  }
+  ngOnDestroy(): void {
+    this.footerService.show();
+  }
 
 }
