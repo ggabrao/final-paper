901763f5351f9b1977d17d60916d967f3a953d12
Gabriel <gabriel.abrao@hotmail.com>
2023-12-08 10:09:32 -0300

    temp

--- portfolio/src/app/footer/footer.component.ts
+++ portfolio/src/app/footer/footer.component.ts
@@ -1,28 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { FooterService } from '../footer.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit, OnDestroy {
+export class FooterComponent {
   myDate: Date;
-  showFooter!: boolean;
-  subscription!: Subscription;
 
-  constructor(private footerService: FooterService) {
+  constructor() {
     this.myDate = new Date();
   }
 
-  ngOnInit(): void {
-    this.subscription = this.footerService.showFooter.subscribe(value => this.showFooter = value);
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 
 
 }
