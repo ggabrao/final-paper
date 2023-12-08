50535790a300a5bc89a55d8b9e258f437780c556
Gabriel <gabriel.abrao@hotmail.com>
2023-12-08 14:22:22 -0300

    hide footer

--- portfolio/src/app/website/website.component.ts
+++ portfolio/src/app/website/website.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FooterService } from '../footer.service';
 
 @Component({
   selector: 'wb-website',
   templateUrl: './website.component.html',
   styleUrl: './website.component.scss'
 })
-export class WebsiteComponent {
+export class WebsiteComponent implements OnInit, OnDestroy {
 
-}
+  constructor(private footerService:FooterService) {}
+  
+    ngOnInit(): void {
+      this.footerService.hide();
+    }
+    ngOnDestroy(): void {
+      this.footerService.show();
+    }
+  
+  }
