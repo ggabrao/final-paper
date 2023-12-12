d951cef937eb00586d023191651768d11b5bcf05
Gabriel G Abrao <gabriel.abrao@hotmail.com>
2023-12-12 10:54:29 -0300

    installed stars library

--- portfolio/src/app/courses/courses.module.ts
+++ portfolio/src/app/courses/courses.module.ts
@@ -6,16 +6,18 @@ import { CoursesComponent } from './courses.component';
 import { DialogFormComponent } from './dialog-form/dialog-form.component';
 import { EditComponent } from './edit/edit.component';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { StarRatingModule } from 'angular-star-rating';
 
 @NgModule({
   declarations: [
-    CoursesComponent,DialogFormComponent,EditComponent
+    CoursesComponent, DialogFormComponent, EditComponent
   ],
   imports: [
     SharedModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    StarRatingModule.forChild()
   ],
-  providers: [{provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}]
-  
+  providers: [{ provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } }]
+
 })
 export class CoursesModule { }
