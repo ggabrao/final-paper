b0234c0a3596b2934a935b3f7187011f10abd7b7
Gabriel <gabriel.abrao@hotmail.com>
2023-12-12 17:58:30 -0300

    theory api

--- portfolio/src/app/courses/edit/edit.component.ts
+++ portfolio/src/app/courses/edit/edit.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CoursesService } from '../courses.service';
 import { ICourse } from '../course.model';
+import { FooterService } from 'src/app/footer.service';
 
 @Component({
   selector: 'crs-edit',
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
-export class EditComponent implements OnInit {
+export class EditComponent implements OnInit, OnDestroy {
 
   selectedCourse!: ICourse;
 
-  constructor(private route: ActivatedRoute, private dataService: CoursesService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private dataService: CoursesService, private router: Router,
+    private footerService: FooterService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(({ courseResolver }) => this.selectedCourse = courseResolver);
+    this.footerService.hide();
+  }
+
+  ngOnDestroy(): void {
+    this.footerService.show();
   }
 
   onCancel() {
