b3a81d6c9bcc2417e2af631d4c7d984fb568033b
Gabriel G Abrao <gabriel.abrao@hotmail.com>
2023-12-17 10:28:34 -0300

    spinner position

--- portfolio/src/app/spinner.component.ts
+++ portfolio/src/app/spinner.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-spinner',
   template: '<mat-spinner class="spinner"></mat-spinner>',
-  styles: '.spinner { position:absolute; top:50%; left:50% }'
+  styles: '.spinner { position:absolute; top:50%; left:50%; transform: translate(-50%,-50%) }'
 })
 export class SpinnerComponent {}
