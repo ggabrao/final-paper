58c4543dc619ecdb95900e3b3459939492b32ae8
Gabriel <gabriel.abrao@hotmail.com>
2023-12-19 09:10:24 -0300

    page not found

--- portfolio/src/app/page-not-found.component.ts
+++ portfolio/src/app/page-not-found.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FooterService } from './footer.service';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  
+
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit, OnDestroy {
+
+  constructor(private footerService: FooterService) { }
+
+  ngOnInit(): void {
+    this.footerService.hide();
+  }
+  ngOnDestroy(): void {
+    this.footerService.show();
+  }
 
 }
