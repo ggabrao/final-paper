5f4704181065202dac4207768e906a04ad2438c1
Gabriel Abrão <gguimaraesabrao@gmail.com>
2025-08-05 09:44:24 -0300

    Add effect to intro text

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent {
+  fullText = 'Hi, I am Gabriel Abrão';
+  displayedText = '';
+  private typingSpeed = 100;
+  private erasingSpeed = 50;
+  private delayBetween = 1500;
+  private isErasing = false;
+  private charIndex = 0;
+  private intervalId: any;
 
+  ngOnInit() {
+    this.startTyping();
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.intervalId);
+  }
+
+  startTyping() {
+    if (!this.isErasing && this.charIndex < this.fullText.length) {
+      this.displayedText += this.fullText.charAt(this.charIndex);
+      this.charIndex++;
+      this.intervalId = setTimeout(() => this.startTyping(), this.typingSpeed);
+    } else if (!this.isErasing && this.charIndex === this.fullText.length) {
+      this.isErasing = true;
+      this.intervalId = setTimeout(() => this.startTyping(), this.delayBetween);
+    } else if (this.isErasing && this.charIndex > 0) {
+      this.displayedText = this.fullText.substring(0, this.charIndex - 1);
+      this.charIndex--;
+      this.intervalId = setTimeout(() => this.startTyping(), this.erasingSpeed);
+    } else {
+      this.isErasing = false;
+      this.intervalId = setTimeout(() => this.startTyping(), this.typingSpeed);
+    }
+  }
 }
+
